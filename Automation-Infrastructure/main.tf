67cac068a5a44891f1f4e1f205e7c5f413292f42
nmn3m <nurmn3m@gmail.com>
2022-12-16 19:18:10 +0200

    1st part

---
+++ Automation-Infrastructure/main.tf
@@ -0,0 +1,48 @@
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+state = "available"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support 
+  enable_dns_hostnames           = var.enable_dns_support
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink
+
+}
+
+# Create public subnets
+resource "aws_subnet" "public" {
+    # if prefetted number equal zero, it will take preferred number as number of valid AZ in us-east-1 otherwise it will take the number of prefered.
+    # we use count(), length(), cidrdsubnet() functions.
+  count  = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets   
+  vpc_id = aws_vpc.main.id
+  # cidrsubnet(prefix, newbits, netnum) 
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4 , count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+}
+
+
+# Create private subnets
+resource "aws_subnet" "private" {
+  count                   = var.preferred_number_of_private_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_private_subnets
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+tags = merge(
+    var.tags,
+    {
+      Name = format("%s-PrivateSubnet-%s", var.name, count.index)
+    },
+  )
+
+}
---
+++ Automation-Infrastructure/variables.tf
@@ -0,0 +1,43 @@
+variable "region" {
+      default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    default = "172.16.0.0/16"
+}
+
+variable "enable_dns_support" {
+    default = "true"
+}
+
+variable "enable_dns_hostnames" {
+    default ="true" 
+}
+
+variable "enable_classiclink" {
+    default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+    default = "false"
+}
+
+variable "preferred_number_of_public_subnets" {
+      default = null
+}
+
+variable "preferred_number_of_private_subnets" {
+      default = null
+}
+
+
+variable "name" {
+    type = string
+    description = "mn3m"
+}
+
+variable "tags" {
+    description = "A mapping of tags assigned to all resources"
+    type = map(string)
+    default = {}
+}
